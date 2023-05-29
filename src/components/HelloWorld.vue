4746f0d068dc7a2d96a13871574e7f73eb550f66
martinapiglio <martina.piglio@gmail.com>
2023-05-29 15:22:56 +0200

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  name: 'App',
+
+  data() {
+    return {
+      message: 'halo'
+    }
+  }
+
+}
+</script>
+
+<template>
+  <h1> {{ message }}</h1>
+</template>
+
+
+<style scoped>
+
+</style>
