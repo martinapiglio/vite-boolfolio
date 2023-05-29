e81d2a84806136280d6556ab5da433c3cce5d046
martinapiglio <martina.piglio@gmail.com>
2023-05-29 15:45:59 +0200

    milestone 3+4: axios installed + API call for project data included + data results shown in console and page (no style)

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,49 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'App',
+
+  data() {
+    return {
+      projects: [],
+    }
+  },
+
+  created() {
+    this.getProjects();
+  },
+
+  methods: {
+    getProjects() {
+      axios.get('http://127.0.0.1:8000/api/projects').then(res => {
+        console.log(res.data.results);
+        this.projects = res.data.results;
+        console.log(this.projects);
+      })
+    }
+  }
+
+}
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>Projects</h1>
+  <table>
+    <thead>
+      <th>Title</th>
+      <th>Desc</th>
+    </thead>
+    <tbody>
+      <tr v-for="project in projects">
+        <td> {{ project.title }}</td>
+        <td> {{ project.description }} </td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
